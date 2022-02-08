98dedc32e4ac47ae3e89a5d56dec5af3ee6f4855
Pablo López <p.lopezmunoz@gmail.com>
2022-02-08 19:24:43 +0100

    finish until bonus 6

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.all('/beers', (req,res)=>{
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+  res.render('beers', beersFromApi)})
+  .catch(error => console.log(error));
+});
+
+app.all('/random_beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI =>{console.log('Random beer from the database: ', responseFromAPI)
+  res.render('random_beer', responseFromAPI[0])})
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
